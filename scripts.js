a15be7b501679226917a5439c0940284b6222c48
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-20 23:44:21 +0200

    Update the edit function

--- scripts.js
+++ scripts.js
@@ -2,21 +2,31 @@
 function toggleEdite () {
   document.getElementById('Edit').onclick = function () {
     const editables = document.querySelectorAll('.editable');
-    const margin = '1rem';
-    const radius = '20px';
-    editables.forEach((ele, index) => {
-      console.log(ele);
-      if (index === 0) {
-        ele.style.margin = `0px ${margin} ${margin}`;
-        ele.style.borderRadius = `0px 0px ${radius} ${radius}`;
-      } else if (index === editables.length - 1) {
-        ele.style.margin = `${margin} ${margin} 0px`;
-        ele.style.borderRadius = `${radius} ${radius} 0px 0px`;
-      } else {
-        ele.style.margin = margin;
-        ele.style.borderRadius = radius;
-      }
-    });
+    if (this.innerHTML === 'Edit') {
+      const margin = '1rem';
+      const radius = '20px';
+
+      this.innerHTML = 'Done';
+      editables.forEach((ele, index) => {
+        console.log(ele);
+        if (index === 0) {
+          ele.style.margin = `0px ${margin} ${margin}`;
+          ele.style.borderRadius = `0px 0px ${radius} ${radius}`;
+        } else if (index === editables.length - 1) {
+          ele.style.margin = `${margin} ${margin} 0px`;
+          ele.style.borderRadius = `${radius} ${radius} 0px 0px`;
+        } else {
+          ele.style.margin = margin;
+          ele.style.borderRadius = radius;
+        }
+      });
+    } else {
+      editables.forEach((ele) => {
+        this.innerHTML = 'Edit';
+        ele.style.margin = '0px';
+        ele.style.borderRadius = '0px';
+      });
+    }
   };
 }
 
